330ed9a5de6776a253e48c97ceea7fd3c995b636
James Carnegie <james@atomist.com>
2018-07-19 11:42:21 +0100

    Stubs taken from sdm-pack-changelog

---
+++ src/xray.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2018 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { subscription } from "@atomist/automation-client/graph/graphQL";
+import {
+    ExtensionPack,
+    Goal,
+} from "@atomist/sdm";
+import { metadata } from "@atomist/sdm/api-helper/misc/extensionPack";
+
+/**
+ * Register support for XRay violations.
+ * Note: If a goal gets passed to this function, this pack will
+ * register a goal implementation for the given goal.
+ * @param {Goal} goal
+ * @returns {ExtensionPack}
+ */
+export function xraySupport(goal?: Goal): ExtensionPack {
+    return {
+        ...metadata(),
+        configure: sdm => {
+
+        },
+    };
+}
